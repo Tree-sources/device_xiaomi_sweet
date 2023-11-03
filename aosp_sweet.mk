83de0e4edfe3dd5d469c0cc191abf4f1fd904116
pure-soul-kk <krishnakripa34567@gmail.com>
2023-11-03 13:09:36 +0530

    sweet: init Project Elixir

--- aosp_sweet.mk
+++ aosp_sweet.mk
@@ -11,9 +11,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common PixelOS stuff.
+# Inherit some common Project Elixir stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Elixir Officials
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+ELIXIR_BUILD_TYPE := OFFICIAL
 
 PRODUCT_NAME := aosp_sweet
 PRODUCT_DEVICE := sweet
